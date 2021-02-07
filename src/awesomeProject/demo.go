5dcd880c3ef6b5f5ad92c89ae6281f4e7db0015d
zhangyue <575865770@qq.com>
2021-02-07 14:45:14 +0800

    Default Changelist

--- src/awesomeProject/demo.go
+++ src/awesomeProject/demo.go
@@ -1,5 +1,4 @@
 package main // 声明 main 包
-import "fmt"
 
 //func main() { // 声明 main 主函数
 //fmt.Println("Hello World!") // 打印 Hello World!
@@ -28,7 +27,8 @@ func main(){
 	fmt.Println(a, b)
 }*/
 
-func main() {
+//局部变量
+/*func main() {
 	//声明局部变量 a和b并赋值
 	var a int = 3
 	var b int = 4
@@ -36,4 +36,51 @@ func main() {
 	//声明局部变量 c 并计算 a 和 b 的和
 	c := a + b
 	fmt.Print("a=%d,b=%d,c=%d\n", a, b, c)
+}*/
+
+//全局变量
+//声明全局变量
+/*var c int
+
+func main() {
+	//声明局部变量
+	var a, b int
+
+	//初始化参数
+	a = 3
+	b = 4
+	c = a + b
+
+	fmt.Printf("a = %d, b = %d, c = %d\n", a, b, c)
+}*/
+
+//形参
+//全局变量
+/*var a int = 13
+
+func main() {
+	//局部变量 a和b
+	var a int = 3
+	var b int = 4
+
+	fmt.Printf("main()函数中 a = %d\n", a)
+	fmt.Printf("main()函数中 b = %d\n", b)
+	c := sum(a, b)
+	fmt.Printf("main() 函数中 c = %d\n", c)
 }
+
+func sum(a, b int) int {
+	fmt.Printf("sum() 函数中 a = %d\n", a)
+	fmt.Printf("sum() 函数中 b = %d\n", b)
+	num := a + b
+	return num
+}*/
+
+/*//复数
+func main() {
+	var x complex128 = complex(1, 2) // 1+2i
+	var y complex128 = complex(3, 4) // 3+4i
+	fmt.Println(x * y)               // "(-5+10i)"
+	fmt.Println(real(x * y))         // "-5"  实部
+	fmt.Println(imag(x * y))         // "10"  虚部
+}*/
---
+++ src/awesomeProject/int.go
@@ -0,0 +1,12 @@
+package main
+
+/*func main() {
+	var f float32 = 16777216 // 1 << 24
+	fmt.Println(f == f+1)    // "true"!
+}
+*/
+
+/*func main() {
+	fmt.Printf("%f\n", math.Pi)
+	fmt.Printf("%.2f\n", math.Pi)
+}*/
