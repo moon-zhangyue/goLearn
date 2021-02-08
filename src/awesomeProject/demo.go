53ca1e5565273a74f608b1550abd1be683dc1bd0
zhangyue <575865770@qq.com>
2021-02-08 11:46:43 +0800

    指针操作

--- src/awesomeProject/demo.go
+++ src/awesomeProject/demo.go
@@ -160,8 +160,58 @@ func main() {
 }*/
 
 //指针
-func main() {
+/*func main() {
 	var cat int = 1
 	var str string = "banana"
 	fmt.Printf("%p %p", &cat, &str)
+}*/
+
+/*func main() {
+
+	// 准备一个字符串类型
+	var house = "Malibu Point 10880, 90265"
+
+	// 对字符串取地址, ptr类型为*string
+	ptr := &house
+
+	// 打印ptr的类型
+	fmt.Printf("ptr type: %T\n", ptr)
+
+	// 打印ptr的指针地址
+	fmt.Printf("address: %p\n", ptr)
+
+	// 对指针进行取值操作
+	value := *ptr
+
+	// 取值后的类型
+	fmt.Printf("value type: %T\n", value)
+
+	// 指针取值后就是指向变量的值
+	fmt.Printf("value: %s\n", value)
+
+}*/
+
+// 交换函数
+func swap(a, b *int) {
+
+	// 取a指针的值, 赋给临时变量t
+	t := *a
+
+	// 取b指针的值, 赋给a指针指向的变量
+	*a = *b
+
+	// 将a指针的值赋给b指针指向的变量
+	*b = t
+}
+
+func main() {
+
+	// 准备两个变量, 赋值1和2
+	x, y := 1, 2
+
+	// 交换变量值
+	swap(&x, &y)
+
+	// 输出变量值
+	fmt.Println(x, y)
 }
