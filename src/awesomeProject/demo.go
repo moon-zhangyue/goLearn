7027501eb167f0dfba2debf4d9b9100248ae2f90
zhangyue <575865770@qq.com>
2021-02-18 13:30:46 +0800

    type关键字

--- src/awesomeProject/demo.go
+++ src/awesomeProject/demo.go
@@ -1,5 +1,4 @@
 package main // 声明 main 包
-import "fmt"
 
 //func main() { // 声明 main 主函数
 //fmt.Println("Hello World!") // 打印 Hello World!
@@ -354,7 +353,7 @@ func main() {
 }*/
 
 // 声明芯片类型
-type ChipType int
+/*type ChipType int
 
 const (
 	Memory ChipType = iota
@@ -378,4 +377,41 @@ func (c ChipType) String() string {
 func main() {
 	// 输出CPU的值并以整型格式显示
 	fmt.Printf("%s %d", Memory, Memory)
+}*/
+
+//type关键字
+//将NewInt定义为int类型
+/*type NewInt int
+
+//将int取一个别名叫IntAlias
+type IntAlias = int
+
+func main() {
+
+	//将a声明为NewInt类型
+	var a NewInt
+	//查看a的类型名
+	fmt.Printf("a type: %T\n", a)
+
+	//将a2声明为IntAlias类型
+	var a2 IntAlias
+	//查看a2的类型名
+	fmt.Printf("a2 type: %T\n", a2)
+}*/
+
+// 定义time.Duration的别名为MyDuration
+/*type MyDuration = time.Duration
+
+// 为MyDuration添加一个函数
+func (m MyDuration) EasySet(a string) {
 }
+func main() {
+}*/
+
+/*
+编译器提示：不能在一个非本地的类型 time.Duration 上定义新方法，非本地类型指的就是 time.Duration 不是在 main 包中定义的，而是在 time 包中定义的，与 main 包不在同一个包中，因此不能为不在一个包中的类型定义方法。
+
+解决这个问题有下面两种方法：
+将第 8 行修改为 type MyDuration time.Duration，也就是将 MyDuration 从别名改为类型；
+将 MyDuration 的别名定义放在 time 包中。
+*/
