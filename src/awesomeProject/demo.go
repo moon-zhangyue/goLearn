2f8026f98e3739a2c3a2311e8ee1031c4852fbf5
zhangyue <575865770@qq.com>
2021-02-22 14:10:47 +0800

    字符串和数字转换

--- src/awesomeProject/demo.go
+++ src/awesomeProject/demo.go
@@ -531,7 +531,7 @@ func main() {
 	}
 }*/
 
-func main() {
+/*func main() {
 	str := "3.1415926"
 	num, err := strconv.ParseFloat(str, 64)
 	if err != nil {
@@ -539,4 +539,17 @@ func main() {
 	} else {
 		fmt.Println(num)
 	}
+}*/
+
+func main() {
+	// 声明一个slice
+	b10 := []byte("int (base 10):")
+
+	// 将转换为10进制的string，追加到slice中
+	b10 = strconv.AppendInt(b10, -42, 10)
+
+	fmt.Println(string(b10))
+	b16 := []byte("int (base 16):")
+	b16 = strconv.AppendInt(b16, -42, 16)
+	fmt.Println(string(b16))
 }
