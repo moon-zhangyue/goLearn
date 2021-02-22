a032fa78094da8608a2f6ad6b576567c2b4003f5
zhangyue <575865770@qq.com>
2021-02-22 17:47:44 +0800

    声明切片

--- src/awesomeProject/demo.go
+++ src/awesomeProject/demo.go
@@ -634,18 +634,38 @@ func main() {
 
 	//fmt.Println(a, a[1:2])
 
-	var highRiseBuilding [30]int
+	//var highRiseBuilding [30]int
 
-	for i := 0; i < 30; i++ {
-		highRiseBuilding[i] = i + 1
-	}
+	//for i := 0; i < 30; i++ {
+	//	highRiseBuilding[i] = i + 1
+	//}
 
 	// 区间
-	fmt.Println(highRiseBuilding[10:15])
+	//fmt.Println(highRiseBuilding[10:15])
 
 	// 中间到尾部的所有元素
-	fmt.Println(highRiseBuilding[20:])
+	//fmt.Println(highRiseBuilding[20:])
 
 	// 开头到中间指定位置的所有元素
-	fmt.Println(highRiseBuilding[:2])
+	//fmt.Println(highRiseBuilding[:2])
+
+	// 声明字符串切片
+	var strList []string
+
+	// 声明整型切片
+	var numList []int
+
+	// 声明一个空切片
+	var numListEmpty = []int{}
+
+	// 输出3个切片
+	fmt.Println(strList, numList, numListEmpty)
+
+	// 输出3个切片大小
+	fmt.Println(len(strList), len(numList), len(numListEmpty))
+
+	// 切片判定空的结果
+	fmt.Println(strList == nil)
+	fmt.Println(numList == nil)
+	fmt.Println(numListEmpty == nil)
 }
