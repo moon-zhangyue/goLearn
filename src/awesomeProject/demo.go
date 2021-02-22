621017a3edfa3141722f9b9690d64028b05c09e3
zhangyue <575865770@qq.com>
2021-02-22 15:57:58 +0800

    数组比较

--- src/awesomeProject/demo.go
+++ src/awesomeProject/demo.go
@@ -556,18 +556,40 @@ func main() {
 var 数组变量名 [元素数量]Type
 */
 func main() {
-	var a [3]int // 定义三个整数的数组
+	//var a [3]int // 定义三个整数的数组
+	//
+	//fmt.Println(a[0])        // 打印第一个元素
+	//fmt.Println(a[len(a)-1]) // 打印最后一个元素
+	//
+	//// 打印索引和元素
+	//for i, v := range a {
+	//	fmt.Printf("%d %d\n", i, v)
+	//}
+	//
+	//// 仅打印元素
+	//for _, v := range a {
+	//	fmt.Printf("%d\n", v)
+	//}
 
-	fmt.Println(a[0])        // 打印第一个元素
-	fmt.Println(a[len(a)-1]) // 打印最后一个元素
+	//var q [3]int = [3]int{1, 2, 3}
+	//var r [3]int = [3]int{1, 2}
+	//fmt.Println(r[2]) // "0"
+	//fmt.Println(q[2]) // "3"
 
-	// 打印索引和元素
-	for i, v := range a {
-		fmt.Printf("%d %d\n", i, v)
-	}
+	//m := [...]int{1, 2, 3}
+	//fmt.Printf("%T\n", m) // "[3]int"
 
-	// 仅打印元素
-	for _, v := range a {
-		fmt.Printf("%d\n", v)
-	}
+	//错误
+	//q := [3]int{1, 2, 3}
+	//q = [4]int{1, 2, 3, 4} // 编译错误：无法将 [4]int 赋给 [3]int
+	//
+	//fmt.Printf("%T\n", q) // "[3]int"
+
+	a := [2]int{1, 2}
+	b := [...]int{1, 2}
+	c := [2]int{1, 3}
+
+	fmt.Println(a == b, a == c, b == c) // "true false false"
+	//d := [3]int{1, 2}
+	//fmt.Println(a == d) // 编译错误：无法比较 [2]int == [3]int
 }
