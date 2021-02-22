da4fc0725654ae6c819e73c3fe5fdd4083d7fa0c
zhangyue <575865770@qq.com>
2021-02-22 16:41:11 +0800

    二维数组

--- src/awesomeProject/demo.go
+++ src/awesomeProject/demo.go
@@ -555,7 +555,7 @@ func main() {
 定义数组
 var 数组变量名 [元素数量]Type
 */
-func main() {
+/*func main() {
 	//var a [3]int // 定义三个整数的数组
 	//
 	//fmt.Println(a[0])        // 打印第一个元素
@@ -601,4 +601,19 @@ func main() {
 	for k, v := range team {
 		fmt.Println(k, v)
 	}
+}*/
+
+//多维数组
+func main() {
+	// 声明一个二维整型数组，两个维度的长度分别是 4 和 2
+	var array [4][2]int
+	// 使用数组字面量来声明并初始化一个二维整型数组
+	array = [4][2]int{{10, 11}, {20, 21}, {30, 31}, {40, 41}}
+	fmt.Printf("%v", array)
+	// 声明并初始化数组中索引为 1 和 3 的元素
+	array = [4][2]int{1: {20, 21}, 3: {40, 41}}
+	fmt.Printf("%v", array)
+	// 声明并初始化数组中指定的元素
+	array = [4][2]int{1: {0: 20}, 3: {1: 41}}
+	fmt.Printf("%v", array)
 }
