b045e23b247ed4f6be939f22c6a35827e8d7cff7
zhangyue <575865770@qq.com>
2021-02-26 17:25:31 +0800

    feat: sync.Map

--- src/awesomeProject/demo.go
+++ src/awesomeProject/demo.go
@@ -1,5 +1,9 @@
 //demo测试
 package main // 声明 main 包
+import (
+	"fmt"
+	"sync"
+)
 
 //func main() { // 声明 main 主函数
 //fmt.Println("Hello World!") // 打印 Hello World!
@@ -906,7 +910,7 @@ var 数组变量名 [元素数量]Type
 数据都基于特征值构建好索引后，就可以进行查询。查询时，重复这个过程，将查询条件转为特征值，使用特征值进行查询得到结果。
 */
 // 人员档案
-type Profile struct {
+/*type Profile struct {
 	Name    string // 名字
 	Age     int    // 年龄
 	Married bool   // 已婚
@@ -920,7 +924,7 @@ func main() {
 	}
 	buildIndex(list)
 	queryData("张三", 30)
-}
+}*/
 
 /*func simpleHash(str string) (ret int) {
 	// 遍历字符串中的每一个ASCII字符
@@ -1044,3 +1048,30 @@ func queryData(name string, age int) {
 非指针：每个指针数值都不同，失去哈希意义。
 函数、闭包不能作为 map 的键。
 */
+
+/*
+sync.Map 有以下特性：
+无须初始化，直接声明即可。
+sync.Map 不能使用 map 的方式进行取值和设置等操作，而是使用 sync.Map 的方法进行调用，Store 表示存储，Load 表示获取，Delete 表示删除。
+使用 Range 配合一个回调函数进行遍历操作，通过回调函数返回内部遍历出来的值，Range 参数中回调函数的返回值在需要继续迭代遍历时，返回 true，终止迭代遍历时，返回 false。
+*/
+func main() {
+	var scene sync.Map
+
+	// 将键值对保存到sync.Map
+	scene.Store("greece", 97)
+	scene.Store("london", 100)
+	scene.Store("egypt", 200)
+
+	// 从sync.Map中根据键取值
+	fmt.Println(scene.Load("london"))
+
+	// 根据键删除对应的键值对
+	scene.Delete("london")
+
+	// 遍历所有sync.Map中的键值对
+	scene.Range(func(k, v interface{}) bool {
+		fmt.Println("iterate:", k, v)
+		return true
+	})
+}
