9e5f9fce92efc90aa0964b3af6820894342ec582
zhangyue <575865770@qq.com>
2021-03-02 17:49:31 +0800

    feat: robot

--- src/awesomeProject/demo.go
+++ src/awesomeProject/demo.go
@@ -1338,7 +1338,7 @@ err != nil 才是 if 的判断表达式，当 err 不为空时，打印错误并
 	}
 }*/
 
-func main() {
+/*func main() {
 
 	//for x := 0; x < 10; x++ {
 	//
@@ -1358,4 +1358,56 @@ func main() {
 	// 标签
 	//breakHere:
 	//	fmt.Println("done")
-}
+
+OuterLoop:
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 5; j++ {
+			switch j {
+			case 2:
+				fmt.Println(i, j)
+				break OuterLoop
+			case 3:
+				fmt.Println(i, j)
+				break OuterLoop
+			}
+		}
+	}
+}*/
+
+/*func main() {
+	// 准备从标准输入读取数据。
+	inputReader := bufio.NewReader(os.Stdin)
+	fmt.Println("Please input your name:")
+	// 读取数据直到碰到 \n 为止。
+	input, err := inputReader.ReadString('\n')
+	if err != nil {
+		fmt.Printf("An error occurred: %s\n", err)
+		// 异常退出。
+		os.Exit(1)
+	} else {
+		// 用切片操作删除最后的 \n 。
+		name := input[:len(input)-2]
+		fmt.Printf("Hello, %s! What can I do for you?\n", name)
+	}
+	for {
+		input, err = inputReader.ReadString('\n')
+		if err != nil {
+			fmt.Printf("An error occurred: %s\n", err)
+			continue
+		}
+		input = input[:len(input)-1]
+		// 全部转换为小写。
+		input = strings.ToLower(input)
+		fmt.Println(input)
+		switch input {
+		case "":
+			continue
+		case "nothing", "bye":
+			fmt.Println("Bye!")
+			// 正常退出。
+			os.Exit(0)
+		default:
+			fmt.Println("Sorry, I didn't catch you.")
+		}
+	}
+}*/
