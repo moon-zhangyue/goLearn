81c7a75f3ddf69d3b0ce8861396338a82eb7f691
zhangyue <575865770@qq.com>
2021-03-03 17:21:51 +0800

    feat: 二分排序

--- src/awesomeProject/demo.go
+++ src/awesomeProject/demo.go
@@ -1,10 +1,6 @@
 //demo测试
 package main // 声明 main 包
-import (
-	"fmt"
-	"sort"
-	"strings"
-)
+import "fmt"
 
 //func main() { // 声明 main 主函数
 //fmt.Println("Hello World!") // 打印 Hello World!
@@ -1529,7 +1525,7 @@ func reportByFrequency(wordsForFrequency map[int][]string) {
 }*/
 
 //缩进排序
-var original = []string{
+/*var original = []string{
 	"Nonmetals",
 	"    Hydrogen",
 	"    Carbon",
@@ -1633,4 +1629,33 @@ func (entries Entries) Less(i, j int) bool {
 }
 func (entries Entries) Swap(i, j int) {
 	entries[i], entries[j] = entries[j], entries[i]
+}*/
+
+//Go语言二分查找法
+//二分查找函数 //假设有序数组的顺序是从小到大（很关键，决定左右方向）
+func BinaryFind(arr *[]int, leftIndex int, rightIndex int, findVal int) {
+	//判断 leftIndex是否大于rightIndex
+	if leftIndex > rightIndex {
+		fmt.Println("找不到")
+		return
+	}
+	//先找到 中间的下标
+	middle := (leftIndex + rightIndex) / 2
+
+	if (*arr)[middle] > findVal {
+		//要查找的数，范围应该在 leftIndex 到 middle+1
+		BinaryFind(arr, leftIndex, middle-1, findVal)
+	} else if (*arr)[middle] < findVal {
+		//要查找的数，范围应该在 middle+1 到 rightIndex
+		BinaryFind(arr, middle+1, rightIndex, findVal)
+	} else {
+		fmt.Printf("找到了，下标为：%v \n", middle)
+	}
+}
+
+func main() {
+	//定义一个数组
+	arr := []int{1, 2, 5, 7, 15, 25, 30, 36, 39, 51, 67, 78, 80, 82, 85, 91, 92, 97}
+	BinaryFind(&arr, 0, len(arr)-1, 30)
+	fmt.Println("main arr=", arr)
 }
