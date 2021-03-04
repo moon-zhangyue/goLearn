331fc2efefd1a7f798481ba4e148f6ef98870d76
zhangyue <575865770@qq.com>
2021-03-04 17:49:06 +0800

    feat: GO字符串处理

--- src/awesomeProject/demo.go
+++ src/awesomeProject/demo.go
@@ -1,5 +1,9 @@
 //demo测试
 package main // 声明 main 包
+import (
+	"fmt"
+	"strings"
+)
 
 //func main() { // 声明 main 主函数
 //fmt.Println("Hello World!") // 打印 Hello World!
@@ -1880,3 +1884,59 @@ func main() {
 	f = fire
 	f()
 }*/
+
+//字符串的链式处理——操作与数据分离的设计技巧
+// 字符串处理函数，传入字符串切片和处理链
+func StringProccess(list []string, chain []func(string) string) {
+
+	// 遍历每一个字符串
+	for index, str := range list {
+
+		// 第一个需要处理的字符串
+		result := str
+
+		// 遍历每一个处理链
+		for _, proc := range chain {
+
+			// 输入一个字符串进行处理，返回数据作为下一个处理链的输入。
+			result = proc(result)
+		}
+
+		// 将结果放回切片
+		list[index] = result
+	}
+}
+
+// 自定义的移除前缀的处理函数
+func removePrefix(str string) string {
+
+	return strings.TrimPrefix(str, "go")
+}
+
+func main() {
+
+	// 待处理的字符串列表
+	list := []string{
+		"go scanner",
+		"go parser",
+		"go compiler",
+		"go printer",
+		"go formater",
+	}
+
+	// 处理函数链
+	chain := []func(string) string{
+		removePrefix,
+		strings.TrimSpace,
+		strings.ToUpper,
+	}
+
+	// 处理字符串
+	StringProccess(list, chain)
+
+	// 输出处理好的字符串
+	for _, str := range list {
+		fmt.Println(str)
+	}
+
+}
