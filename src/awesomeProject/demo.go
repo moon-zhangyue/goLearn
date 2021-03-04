b611bdfc2e39be4c530ec080d3923f30b61b91c0
zhangyue <575865770@qq.com>
2021-03-04 14:47:04 +0800

    feat: 函数创建

--- src/awesomeProject/demo.go
+++ src/awesomeProject/demo.go
@@ -1742,3 +1742,37 @@ func main() {
 	}
 	fmt.Println(id)
 }*/
+
+/*func hypot(x, y float64) float64 {
+	return math.Sqrt(x*x + y*y)
+}
+func add(x int, y int) int   { return x + y }
+func sub(x, y int) (z int)   { z = x - y; return }
+func first(x int, _ int) int { return x }
+func zero(int, int) int      { return 0 }
+
+func main() {
+	fmt.Println(hypot(3, 4))  // "5"fmt.Printf("%T\n", add) // "func(int, int) int"
+	fmt.Printf("%T\n", add) // "func(int, int) int"
+	fmt.Printf("%T\n", sub)   // "func(int, int) int"
+	fmt.Printf("%T\n", first) // "func(int, int) int"
+	fmt.Printf("%T\n", zero)  // "func(int, int) int"
+}*/
+/*func typedTwoValues() (int, int) {
+	return 1, 2
+}
+
+func namedRetValues() (a, b int) {
+
+	a = 1
+	b = 2
+
+	return
+}
+
+func main() {
+	a, b := typedTwoValues()
+	c, d := namedRetValues()
+	fmt.Println(a, b)
+	fmt.Println(c, d)
+}*/
