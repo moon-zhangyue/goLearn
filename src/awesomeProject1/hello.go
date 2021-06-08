04b124ee25a644136a2cf1853f708c06d49ae7eb
zhangyue <575865770@qq.com>
2021-06-08 18:48:46 +0800

    feat: 数组遍历

--- src/awesomeProject1/hello.go
+++ src/awesomeProject1/hello.go
@@ -184,10 +184,59 @@ func main() {
 	//fmt.Println(v2)
 	//fmt.Println(v4)
 
-	v1 := []byte{'h', 'e', 'l', 'l', 'o'}
-	v2 := string(v1) // v2 = hello
-	v3 := []rune{0x5b66, 0x9662, 0x541b}
-	v4 := string(v3) // v4 = 学院君
-	fmt.Println(v2)
-	fmt.Println(v4)
+	//v1 := []byte{'h', 'e', 'l', 'l', 'o'}
+	//v2 := string(v1) // v2 = hello
+	//v3 := []rune{0x5b66, 0x9662, 0x541b}
+	//v4 := string(v3) // v4 = 学院君
+	//fmt.Println(v2)
+	//fmt.Println(v4)
+
+	//v1 := "100"
+	//v2, _ := strconv.Atoi(v1) // 将字符串转化为整型，v2 = 100
+	//v3 := 100
+	//v4 := strconv.Itoa(v3) // 将整型转化为字符串, v4 = "100"
+	//v5 := "true"
+	//v6, _ := strconv.ParseBool(v5) // 将字符串转化为布尔型
+	//v5 = strconv.FormatBool(v6)    // 将布尔值转化为字符串
+	//v7 := "100"
+	//v8, _ := strconv.ParseInt(v7, 10, 64)  // 将字符串转化为整型，第二个参数表示进制，第三个参数表示最大位数
+	//v7 = strconv.FormatInt(v8, 10)         // 将整型转化为字符串，第二个参数表示进制
+	//v9, _ := strconv.ParseUint(v7, 10, 64) // 将字符串转化为无符号整型，参数含义同 ParseInt
+	//v7 = strconv.FormatUint(v9, 10)        // 将字符串转化为无符号整型，参数含义同 FormatInt
+	//v10 := "99.99"
+	//v11, _ := strconv.ParseFloat(v10, 64) // 将字符串转化为浮点型，第二个参数表示精度
+	//v10 = strconv.FormatFloat(v11, 'E', -1, 64)
+	//q := strconv.Quote("Hello, 世界")       // 为字符串加引号
+	//q = strconv.QuoteToASCII("Hello, 世界") // 将字符串转化为 ASCII 编码
+	//
+	//fmt.Println(v2)
+	//fmt.Println(v4)
+	//fmt.Println(v5)
+	//fmt.Println(v6)
+	//fmt.Println(v7)
+	//fmt.Println(v8)
+	//fmt.Println(v9)
+	//fmt.Println(v11)
+	//fmt.Println(v10)
+	//fmt.Println(q)
+
+	var a [8]byte           // 长度为8的数组，每个元素为一个字节
+	var b [3][3]int         // 二维数组（9宫格）
+	var c [3][3][3]float64  // 三维数组（立体的9宫格）
+	var d = [3]int{1, 2, 3} // 声明时初始化
+	var e = new([3]string)  // 通过 new 初始化
+
+	m := [5]int{1, 2, 3}
+	fmt.Println(m)
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+
+	var arr = [5]int{1, 2, 3, 4, 5}
+	for i, v := range arr {
+		fmt.Println("Element", i, "of arr is", v)
+	}
 }
