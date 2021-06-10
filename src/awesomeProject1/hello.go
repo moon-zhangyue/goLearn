fae9c72026a39e6b6aec29b73c650c6b932cd6bb
zhangyue <575865770@qq.com>
2021-06-10 16:55:31 +0800

    指针操作

--- src/awesomeProject1/hello.go
+++ src/awesomeProject1/hello.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"unsafe"
 )
 
 //func main() {
@@ -410,9 +411,28 @@ func main() {
 	//	fmt.Println(map1[v], v)
 	//}
 
-	a := 100
-	var ptr *int // 声明指针类型
-	ptr = &a     // 初始化指针类型值为变量 a
-	fmt.Println(ptr)
-	fmt.Println(*ptr)
+	//a := 100
+	//var ptr *int // 声明指针类型
+	//fmt.Println(ptr)
+	//
+	//ptr = &a     // 初始化指针类型值为变量 a
+	//fmt.Println(ptr)
+	//fmt.Println(*ptr)
+
+	//a := 100
+	//ptr := &a
+	//fmt.Printf("%p\n", ptr)
+	//fmt.Printf("%d\n", *ptr)
+
+	i := 10
+	var p *int = &i
+	var fp *float32 = (*float32)(unsafe.Pointer(p))
+	*fp = *fp * 10
+	fmt.Println(i) // 100
+
+	arr := [3]int{1, 2, 3}
+	ap := &arr
+	sp := (*int)(unsafe.Pointer(uintptr(unsafe.Pointer(ap)) + unsafe.Sizeof(arr[0])))
+	*sp += 3
+	fmt.Println(arr)
 }
