af3e1365695426fd070a15ba0a08fd7aa93b42fa
zhangyue <575865770@qq.com>
2021-06-10 18:43:42 +0800

    feat: 循环

--- src/awesomeProject1/hello.go
+++ src/awesomeProject1/hello.go
@@ -479,9 +479,19 @@ func main() {
 	//}
 	//fmt.Println(sum)
 
-	a := []int{1, 2, 3, 4, 5, 6}
-	for i, j := 0, len(a)-1; i < j; i, j = i+1, j-1 {
-		a[i], a[j] = a[j], a[i]
+	//a := []int{1, 2, 3, 4, 5, 6}
+	//for i, j := 0, len(a)-1; i < j; i, j = i+1, j-1 {
+	//	a[i], a[j] = a[j], a[i]
+	//}
+	//fmt.Println(a)
+
+JLoop:
+	for j := 0; j < 5; j++ {
+		for i := 0; i < 10; i++ {
+			if i > 5 {
+				break JLoop
+			}
+			fmt.Println(i)
+		}
 	}
-	fmt.Println(a)
 }
