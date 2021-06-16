58010da2ce70c99b9599ad9961dbb0fec039e090
zhangyue <575865770@qq.com>
2021-06-16 17:22:52 +0800

    泛型 多返回值

--- src/test/main.go
+++ src/test/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"reflect"
 )
 
 //func add(a, b int) int {
@@ -28,31 +28,52 @@ import (
 //	}
 //}
 
-func myPrintf(args ...interface{}) {
-	for _, arg := range args {
-		switch reflect.TypeOf(arg).Kind() {
-		case reflect.Int:
-			fmt.Println(arg, "is an int value.")
-		case reflect.String:
-			fmt.Printf("\"%s\" is a string value.\n", arg)
-		case reflect.Array:
-			fmt.Println(arg, "is an array type.")
-		default:
-			fmt.Println(arg, "is an unknown type.")
-		}
+//func myPrintf(args ...interface{}) {
+//	for _, arg := range args {
+//		switch reflect.TypeOf(arg).Kind() {
+//		case reflect.Int:
+//			fmt.Println(arg, "is an int value.")
+//		case reflect.String:
+//			fmt.Printf("\"%s\" is a string value.\n", arg)
+//		case reflect.Array:
+//			fmt.Println(arg, "is an array type.")
+//		default:
+//			fmt.Println(arg, "is an unknown type.")
+//		}
+//	}
+//}
+
+//func main() {
+//	//x, y := 1, 2
+//	//z := add(&x, &y)
+//	//fmt.Printf("add(%d, %d) = %d\n", x, y, z)
+//
+//	//myfunc(1, 12, 3, 45)
+//
+//	//slice := []int{1, 2, 3, 4, 5}
+//	//myfunc(slice...)
+//	//myfunc(slice[1:3]...)
+//
+//	myPrintf(1, "1", [1]int{1}, true)
+//}
+
+func add(a, b *int) (int, error) {
+	if *a < 0 || *b < 0 {
+		err := errors.New("只支持非负整数相加")
+		return 0, err
 	}
+
+	*a *= 2
+	*b *= 3
+	return *a + *b, nil
 }
 
 func main() {
-	//x, y := 1, 2
-	//z := add(&x, &y)
-	//fmt.Printf("add(%d, %d) = %d\n", x, y, z)
-
-	//myfunc(1, 12, 3, 45)
-
-	//slice := []int{1, 2, 3, 4, 5}
-	//myfunc(slice...)
-	//myfunc(slice[1:3]...)
-
-	myPrintf(1, "1", [1]int{1}, true)
+	x, y := 1, 2
+	z, err := add(&x, &y)
+	if err != nil {
+		fmt.Printf(err.Error())
+		return
+	}
+	fmt.Printf("add(%d, %d) = %d\n", x, y, z)
 }
