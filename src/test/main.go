5e8f69621ebdce7376f85f44a8eeb48780cd80ca
zhangyue <575865770@qq.com>
2021-06-16 16:47:59 +0800

    泛型

--- src/test/main.go
+++ src/test/main.go
@@ -18,8 +18,21 @@ func add(a, b *int) int {
 	*b *= 3
 	return *a + *b
 }
+
+func myfunc(numbers ...int) {
+	for _, number := range numbers {
+		fmt.Println(number)
+	}
+}
+
 func main() {
-	x, y := 1, 2
-	z := add(&x, &y)
-	fmt.Printf("add(%d, %d) = %d\n", x, y, z)
+	//x, y := 1, 2
+	//z := add(&x, &y)
+	//fmt.Printf("add(%d, %d) = %d\n", x, y, z)
+
+	//myfunc(1, 12, 3, 45)
+
+	slice := []int{1, 2, 3, 4, 5}
+	myfunc(slice...)
+	myfunc(slice[1:3]...)
 }
