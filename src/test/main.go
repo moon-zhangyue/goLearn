4ac3f18d3b30ea46eb14de7ff09fe30274ccf406
zhangyue <575865770@qq.com>
2021-06-16 21:33:20 +0800

    refactor: 装饰器模式

--- src/test/main.go
+++ src/test/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 //func add(a, b int) int {
 //	a *= 2
@@ -126,14 +129,42 @@ import "fmt"
 //}
 
 // 将函数作为返回值类型
-func deferAdd(a, b int) func() int {
-	return func() int {
-		return a + b
+//func deferAdd(a, b int) func() int {
+//	return func() int {
+//		return a + b
+//	}
+//}
+//func main() {
+//	// 此时返回的是匿名函数
+//	addFunc := deferAdd(1, 2)
+//	// 这里才会真正执行加法操作
+//	fmt.Println(addFunc())
+//}
+
+// 为函数类型设置别名提高代码可读性
+type MultiPlyFunc func(int, int) int
+
+// 乘法运算函数
+func multiply(a, b int) int {
+	return a * b
+}
+
+// 通过高阶函数在不侵入原有函数实现的前提下计算乘法函数执行时间
+func execTime(f MultiPlyFunc) MultiPlyFunc {
+	return func(a, b int) int {
+		start := time.Now()      // 起始时间
+		c := f(a, b)             // 执行乘法运算函数
+		end := time.Since(start) // 函数执行完毕耗时
+		fmt.Printf("--- 执行耗时: %v ---\n", end)
+		return c // 返回计算结果
 	}
 }
 func main() {
-	// 此时返回的是匿名函数
-	addFunc := deferAdd(1, 2)
-	// 这里才会真正执行加法操作
-	fmt.Println(addFunc())
+	a := 2
+	b := 8
+	// 通过修饰器调用乘法函数，返回的是一个匿名函数
+	decorator := execTime(multiply)
+	// 执行修饰器返回函数
+	c := decorator(a, b)
+	fmt.Printf("%d x %d = %d\n", a, b, c)
 }
