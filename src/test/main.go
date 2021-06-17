bc45113c466e1640d9ff0481e85ef73e055faa9a
zhangyue <575865770@qq.com>
2021-06-17 15:24:49 +0800

    内存优化

--- src/test/main.go
+++ src/test/main.go
@@ -170,39 +170,129 @@ import (
 //}
 
 // 为函数类型设置别名提高代码可读性
-type MultiPlyFunc func(int, int) int
-
-// 乘法运算函数1（算术运算）
-func multiply1(a, b int) int {
-	return a * b
-}
+//type MultiPlyFunc func(int, int) int
+//
+//// 乘法运算函数1（算术运算）
+//func multiply1(a, b int) int {
+//	return a * b
+//}
 
 // 乘法运算函数2（位运算）
-func multiply2(a, b int) int {
-	return a << b
+//func multiply2(a, b int) int {
+//	return a << b
+//}
+//
+//// 通过高阶函数在不侵入原有函数实现的前提下计算乘法函数执行时间
+//func execTime(f MultiPlyFunc) MultiPlyFunc {
+//	return func(a, b int) int {
+//		start := time.Now()      // 起始时间
+//		c := f(a, b)             // 执行乘法运算函数
+//		end := time.Since(start) // 函数执行完毕耗时
+//		fmt.Printf("--- 执行耗时: %v ---\n", end)
+//		return c // 返回计算结果
+//	}
+//}
+//func main() {
+//	a := 2
+//	b := 8
+//	fmt.Println("算术运算：")
+//	decorator1 := execTime(multiply1)
+//	c := decorator1(a, b)
+//	fmt.Printf("%d x %d = %d\n", a, b, c)
+//	fmt.Println("位运算：") //左移是乘以2的N次幂 右移是除以
+//	decorator2 := execTime(multiply2)
+//	a = 1
+//	b = 4
+//	c = decorator2(a, b)
+//	fmt.Printf("%d << %d = %d\n", a, b, c)
+//}
+
+//func fibonacci(n int) int {
+//	if n == 1 {
+//		return 0
+//	}
+//	if n == 2 {
+//		return 1
+//	}
+//	return fibonacci(n-1) + fibonacci(n-2)
+//}
+//
+//func main() {
+//	n := 5
+//	num := fibonacci(n)
+//	fmt.Println(n)
+//	fmt.Printf("The %dth number of fibonacci sequence is %d\n", n, num)
+//}
+
+type FibonacciFunc func(int) int
+
+//通过递归函数实现斐波那契数列
+func fibonacci(n int) int {
+	// 终止条件
+	if n == 1 {
+		return 0
+	}
+	if n == 2 {
+		return 1
+	}
+	// 递归公式
+	return fibonacci(n-1) + fibonacci(n-2)
 }
 
-// 通过高阶函数在不侵入原有函数实现的前提下计算乘法函数执行时间
-func execTime(f MultiPlyFunc) MultiPlyFunc {
-	return func(a, b int) int {
+//// 斐波那契函数执行耗时计算
+func fibonacciExecTime(f FibonacciFunc) FibonacciFunc {
+	return func(n int) int {
 		start := time.Now()      // 起始时间
-		c := f(a, b)             // 执行乘法运算函数
+		num := f(n)              // 执行斐波那契函数
 		end := time.Since(start) // 函数执行完毕耗时
 		fmt.Printf("--- 执行耗时: %v ---\n", end)
-		return c // 返回计算结果
+		return num // 返回计算结果
 	}
 }
+
+//func main() {
+//	n1 := 5
+//	f := fibonacciExecTime(fibonacci)
+//	r1 := f(n1)
+//	fmt.Printf("The %dth number of fibonacci sequence is %d\n", n1, r1)
+//	n2 := 50
+//	r2 := f(n2)
+//	fmt.Printf("The %dth number of fibonacci sequence is %d\n", n2, r2)
+//}
+
+const MAX = 50
+
+var fibs [MAX]int
+
+// 缓存中间结果的递归函数优化版
+func fibonacci2(n int) int {
+	if n == 1 {
+		return 0
+	}
+	if n == 2 {
+		return 1
+	}
+	index := n - 1
+	if fibs[index] != 0 {
+		return fibs[index]
+	}
+	num := fibonacci2(n-1) + fibonacci2(n-2)
+	fibs[index] = num
+
+	fmt.Println(fibs)
+
+	return num
+}
+
 func main() {
-	a := 2
-	b := 8
-	fmt.Println("算术运算：")
-	decorator1 := execTime(multiply1)
-	c := decorator1(a, b)
-	fmt.Printf("%d x %d = %d\n", a, b, c)
-	fmt.Println("位运算：") //左移是乘以2的N次幂 右移是除以
-	decorator2 := execTime(multiply2)
-	a = 1
-	b = 4
-	c = decorator2(a, b)
-	fmt.Printf("%d << %d = %d\n", a, b, c)
+	n1 := 5
+	f1 := fibonacciExecTime(fibonacci)
+	r1 := f1(n1)
+	fmt.Printf("The %dth number of fibonacci sequence is %d\n", n1, r1)
+	n2 := 50
+	r2 := f1(n2)
+	fmt.Printf("The %dth number of fibonacci sequence is %d\n", n2, r2)
+	f2 := fibonacciExecTime(fibonacci2)
+	r3 := f2(n2)
+	fmt.Printf("The %dth number of fibonacci sequence is %d\n", n2, r3)
 }
