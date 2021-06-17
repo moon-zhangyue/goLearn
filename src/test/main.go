27f9cfc87fbd87f0234321738b594a3c8b66167e
zhangyue <575865770@qq.com>
2021-06-17 21:46:02 +0800

    refactor: filter

--- src/test/main.go
+++ src/test/main.go
@@ -345,6 +345,17 @@ func fieldSum(items []string, f func(string) int) int {
 	return sum
 }
 
+// Filter 函数
+func itemsFilter(items []map[string]string, f func(map[string]string) bool) []map[string]string {
+	newSlice := make([]map[string]string, len(items))
+	for _, item := range items {
+		if f(item) {
+			newSlice = append(newSlice, item)
+		}
+	}
+	return newSlice
+}
+
 func main() {
 	var users = []map[string]string{
 		{
@@ -359,15 +370,39 @@ func main() {
 			"name": "王五",
 			"age":  "20",
 		},
+		{
+			"name": "赵六",
+			"age":  "-10",
+		},
+		{
+			"name": "孙七",
+			"age":  "60",
+		},
+		{
+			"name": "周八",
+			"age":  "10",
+		},
 	}
-
-	ageSlice := mapToString(users, func(user map[string]string) string {
+	validUsers := itemsFilter(users, func(user map[string]string) bool {
+		age, ok := user["age"]
+		if !ok {
+			return false
+		}
+		intAge, err := strconv.Atoi(age)
+		if err != nil {
+			return false
+		}
+		if intAge < 18 || intAge > 35 {
+			return false
+		}
+		return true
+	})
+	ageSlice := mapToString(validUsers, func(user map[string]string) string {
 		return user["age"]
 	})
 	sum := fieldSum(ageSlice, func(age string) int {
 		intAge, _ := strconv.Atoi(age)
 		return intAge
 	})
-
 	fmt.Printf("用户年龄累加结果: %d\n", sum)
 }
