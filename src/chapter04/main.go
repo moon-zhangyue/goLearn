2b6c4e53b75d316893f33bb49bd6300feb012dd5
zhangyue <575865770@qq.com>
2021-06-22 16:16:21 +0800

    接口

--- src/chapter04/main.go
+++ src/chapter04/main.go
@@ -1,26 +1,39 @@
 package main
 
-import (
-	. "chapter04/animal"
-	"fmt"
-)
+import "fmt"
 
-func main() {
-	//animal := Animal{Name: "中华田园犬"}
-	//pet := Pet{Name: "宠物狗"}
-	//dog := Dog{Animal: &animal, Pet: pet}
-	//
-	//fmt.Println(dog.Animal.GetName())
-	//fmt.Print(dog.Animal.Call())
-	//fmt.Println(dog.Call())
-	//fmt.Print(dog.Animal.FavorFood())
-	//fmt.Println(dog.FavorFood())
+//func main() {
+//	//animal := Animal{Name: "中华田园犬"}
+//	//pet := Pet{Name: "宠物狗"}
+//	//dog := Dog{Animal: &animal, Pet: pet}
+//	//
+//	//fmt.Println(dog.Animal.GetName())
+//	//fmt.Print(dog.Animal.Call())
+//	//fmt.Println(dog.Call())
+//	//fmt.Print(dog.Animal.FavorFood())
+//	//fmt.Println(dog.FavorFood())
+//
+//	animal := NewAnimal("中华田园犬")
+//	pet := NewPet("宠物狗")
+//	dog := NewDog(&animal, pet)
+//
+//	fmt.Println(dog.GetName())
+//	fmt.Println(dog.Call())
+//	fmt.Println(dog.FavorFood())
+//}
 
-	animal := NewAnimal("中华田园犬")
-	pet := NewPet("宠物狗")
-	dog := NewDog(&animal, pet)
+type A interface {
+	Foo()
+}
+type B interface {
+	A
+	Bar()
+}
+type T struct{}
 
-	fmt.Println(dog.GetName())
-	fmt.Println(dog.Call())
-	fmt.Println(dog.FavorFood())
+func (t T) Foo() {
+	fmt.Println("call Foo function from interface A.")
+}
+func (t T) Bar() {
+	fmt.Println("call Bar function from interface B.")
 }
