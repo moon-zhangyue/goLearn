1a2e92e23dbfaed08be7fe7e32025d032faabdc8
zhangyue <575865770@qq.com>
2021-06-23 17:10:33 +0800

    接口传递值

--- src/chapter04/main.go
+++ src/chapter04/main.go
@@ -22,18 +22,45 @@ import "fmt"
 //	fmt.Println(dog.FavorFood())
 //}
 
-type A interface {
-	Foo()
-}
-type B interface {
-	A
-	Bar()
-}
-type T struct{}
+//type A interface {
+//	Foo()
+//}
+//type B interface {
+//	A
+//	Bar()
+//}
+//type T struct{}
+//
+//func (t T) Foo() {
+//	fmt.Println("call Foo function from interface A.")
+//}
+//func (t T) Bar() {
+//	fmt.Println("call Bar function from interface B.")
+//}
 
-func (t T) Foo() {
-	fmt.Println("call Foo function from interface A.")
+type Integer int
+
+// 加法运算
+func (a Integer) Add(b Integer) Integer {
+	return a + b
 }
-func (t T) Bar() {
-	fmt.Println("call Bar function from interface B.")
+
+// 乘法运算
+func (a Integer) Multiply(b Integer) Integer {
+	return a * b
+}
+
+type Math interface {
+	Add(i Integer) Integer
+	Multiply(i Integer) Integer
+}
+
+func main() {
+	var a Integer = 1
+	var m Math = a
+	fmt.Println(m.Add(1))
+
+	var b Integer = 1
+	var n Math = &b //传递指针
+	fmt.Println(n.Add(1))
 }
