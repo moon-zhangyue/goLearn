f0032abb8f738abd5a5276333269d7bc9637ff32
zhangyue <575865770@qq.com>
2021-07-06 20:48:30 +0800

    refactor: panic

--- src/chapter04/test1.go
+++ src/chapter04/test1.go
@@ -6,12 +6,24 @@ func printError() {
 	fmt.Println("兜底执行")
 }
 func main() {
-	defer printError()
+	//defer printError()
+	//defer func() {
+	//	fmt.Println("除数不能是0!")
+	//}()
+	//var i = 1
+	//var j = 0
+	//var k = i / j
+	//fmt.Printf("%d / %d = %d\n", i, j, k)
+
 	defer func() {
-		fmt.Println("除数不能是0!")
+		fmt.Println("代码清理逻辑")
 	}()
 	var i = 1
 	var j = 0
-	var k = i / j
+	if j == 0 {
+		panic("除数不能为0！")
+	}
+	k := i / j
 	fmt.Printf("%d / %d = %d\n", i, j, k)
+
 }
