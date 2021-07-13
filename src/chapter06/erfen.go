8036ee97d957f993f48e5278c4b6c021b5982ed0
zhangyue <575865770@qq.com>
2021-07-13 17:10:17 +0800

    二分查找

--- src/chapter06/erfen.go
+++ src/chapter06/erfen.go
@@ -1,38 +1,65 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
+import "fmt"
 
 // 二分查找实现代码
-func binarySearch(nums []int, num int, low int, high int) int {
-	// 递归终止条件
+//func binarySearch(nums []int, num int, low int, high int) int {
+//	// 递归终止条件
+//	if low > high {
+//		return -1
+//	}
+//	// 通过中间元素进行二分查找
+//	mid := (low + high) / 2
+//	// 递归查找
+//	if num > nums[mid] {
+//		// 如果待查找数据大于中间元素，则在右区间查找
+//		return binarySearch(nums, num, mid+1, high)
+//	} else if num < nums[mid] {
+//		// 如果待查找数据小于中间元素，则在左区间查找
+//		return binarySearch(nums, num, low, mid-1)
+//	} else {
+//		// 找到了，返回索引值
+//		return mid
+//	}
+//}
+//func main() {
+//	nums := []int{4, 6, 5, 3, 1, 8, 2, 7}
+//	sort.Ints(nums) // 先对待排序数据序列进行排序
+//	fmt.Printf("Sorted nums: %v\n", nums)
+//	num := 5
+//	index := binarySearch(nums, num, 0, len(nums)-1)
+//	if index != -1 {
+//		fmt.Printf("Find num %d at index %d\n", num, index)
+//	} else {
+//		fmt.Printf("Num %d not exists in nums\n", num)
+//	}
+//}
+
+// 二分查找变形版本1：查找第一个值等于给定值的元素
+func binarySearchV1(nums []int, num, low, high int) int {
 	if low > high {
 		return -1
 	}
-	// 通过中间元素进行二分查找
 	mid := (low + high) / 2
-	// 递归查找
-	if num > nums[mid] {
-		// 如果待查找数据大于中间元素，则在右区间查找
-		return binarySearch(nums, num, mid+1, high)
-	} else if num < nums[mid] {
-		// 如果待查找数据小于中间元素，则在左区间查找
-		return binarySearch(nums, num, low, mid-1)
+	if num < nums[mid] {
+		return binarySearchV1(nums, num, low, mid-1)
+	} else if num > nums[mid] {
+		return binarySearchV1(nums, num, mid+1, high)
 	} else {
-		// 找到了，返回索引值
-		return mid
+		// 除了需要判断第一个与 num 相等的元素索引外，其他和普通二分查找逻辑一致
+		if mid == 0 || nums[mid-1] != num {
+			return mid
+		} else {
+			return binarySearchV1(nums, num, low, mid-1)
+		}
 	}
 }
 func main() {
-	nums := []int{4, 6, 5, 3, 1, 8, 2, 7}
-	sort.Ints(nums) // 先对待排序数据序列进行排序
-	fmt.Printf("Sorted nums: %v\n", nums)
-	num := 5
-	index := binarySearch(nums, num, 0, len(nums)-1)
+	nums := []int{1, 2, 3, 3, 4, 5, 6}
+	num := 3
+	index := binarySearchV1(nums, num, 0, len(nums)-1)
 	if index != -1 {
-		fmt.Printf("Find num %d at index %d\n", num, index)
+		fmt.Printf("Find num %d first at index %d\n", num, index)
 	} else {
 		fmt.Printf("Num %d not exists in nums\n", num)
 	}
