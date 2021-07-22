eb6cf860b6bfb707cda0c43b9705c1ea28a3905c
zhangyue <575865770@qq.com>
2021-07-22 21:44:36 +0800

    协程--共享内存

--- src/xiecheng/add.go
+++ src/xiecheng/add.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
 //func add(a, b int) {
 //	var c = a + b
 //	fmt.Printf("%d + %d = %d\n", a, b, c)
@@ -15,14 +10,14 @@ import (
 //	time.Sleep(1e9)
 //}
 
-func add(a, b int) {
-	var c = a + b
-	fmt.Printf("%d + %d = %d\n", a, b, c)
-}
-
-func main() {
-	for i := 0; i < 10; i++ {
-		go add(1, i)
-	}
-	time.Sleep(3e9)
-}
+//func add(a, b int) {
+//	var c = a + b
+//	fmt.Printf("%d + %d = %d\n", a, b, c)
+//}
+//
+//func main() {
+//	for i := 0; i < 10; i++ {
+//		go add(1, i)
+//	}
+//	time.Sleep(3e9)
+//}
