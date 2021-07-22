edd4c3302e698e61843ae2fd38ad13cc32a32ed6
zhangyue <575865770@qq.com>
2021-07-22 21:26:08 +0800

    refactor(协程的创建):

---
+++ src/xiecheng/add.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+//func add(a, b int) {
+//	var c = a + b
+//	fmt.Printf("%d + %d = %d\n", a, b, c)
+//}
+//
+//func main() {
+//	go add(1, 2)
+//	time.Sleep(1e9)
+//}
+
+func add(a, b int) {
+	var c = a + b
+	fmt.Printf("%d + %d = %d\n", a, b, c)
+}
+
+func main() {
+	for i := 0; i < 10; i++ {
+		go add(1, i)
+	}
+	time.Sleep(3e9)
+}
