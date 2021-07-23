07238eccf0443f64b64e80d8f955fe15ce7fc987
zhangyue <575865770@qq.com>
2021-07-23 10:56:13 +0800

    refactor(缓冲通道 无缓冲通道):

--- src/xiecheng/channel.go
+++ src/xiecheng/channel.go
@@ -5,25 +5,46 @@ import (
 	"time"
 )
 
-func add(a, b int, ch chan int) {
-	c := a + b
-	fmt.Printf("%d + %d = %d\n", a, b, c)
-	ch <- 1
+//func add(a, b int, ch chan int) {
+//	c := a + b
+//	fmt.Printf("%d + %d = %d\n", a, b, c)
+//	ch <- 1
+//}
+//
+//func main() {
+//	start := time.Now()
+//	chs := make([]chan int, 10)
+//
+//	for i := 0; i < 10; i++ {
+//		chs[i] = make(chan int)
+//		go add(1, i, chs[i])
+//	}
+//
+//	for _, ch := range chs {
+//		<-ch
+//	}
+//
+//	end := time.Now()
+//	consume := end.Sub(start).Seconds()
+//	fmt.Println("程序执行耗时(s)：", consume)
+//}
+
+//缓冲通道
+func test(ch chan int) {
+	for i := 0; i < 100; i++ {
+		ch <- i
+	}
+	close(ch) //关闭通道
 }
 
 func main() {
 	start := time.Now()
-	chs := make([]chan int, 10)
-
-	for i := 0; i < 10; i++ {
-		chs[i] = make(chan int)
-		go add(1, i, chs[i])
+	ch := make(chan int, 20) //缓冲通道
+	//ch := make(chan int) //非缓冲通道
+	go test(ch)
+	for i := range ch {
+		fmt.Println("接收到的数据:", i)
 	}
-
-	for _, ch := range chs {
-		<-ch
-	}
-
 	end := time.Now()
 	consume := end.Sub(start).Seconds()
 	fmt.Println("程序执行耗时(s)：", consume)
