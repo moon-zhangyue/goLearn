ccb7b653f3db4a4fa301f79292deb54086641e28
zhangyue <575865770@qq.com>
2021-07-25 08:45:52 +0800

    refactor(协程的创建): 错误处理

--- src/xiecheng/channel.go
+++ src/xiecheng/channel.go
@@ -1,11 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
-
 //func add(a, b int, ch chan int) {
 //	c := a + b
 //	fmt.Printf("%d + %d = %d\n", a, b, c)
@@ -197,48 +191,48 @@ import (
 //	}
 //}
 
-func main() {
-	chs := [3]chan int{
-		make(chan int, 3),
-		make(chan int, 3),
-		make(chan int, 3),
-	}
-
-	rand.Seed(time.Now().UnixNano())
-	index1 := rand.Intn(3) // 随机生成0-2之间的数字
-	fmt.Printf("随机索引/数值: %d\n", index1)
-	chs[index1] <- rand.Int() // 向通道发送随机数字
-
-	rand.Seed(time.Now().UnixNano())
-	index2 := rand.Intn(3)
-	fmt.Printf("随机索引/数值: %d\n", index2)
-	chs[index2] <- rand.Int()
-
-	rand.Seed(time.Now().UnixNano())
-	index3 := rand.Intn(3)
-	fmt.Printf("随机索引/数值: %d\n", index3)
-	chs[index3] <- rand.Int()
-
-	// 哪一个通道中有值，哪个对应的分支就会被执行
-	for i := 0; i < 3; i++ {
-		select {
-		case num, ok := <-chs[0]:
-			if !ok {
-				break
-			}
-			fmt.Println("第一个条件分支被选中: chs[0]=>", num)
-		case num, ok := <-chs[1]:
-			if !ok {
-				break
-			}
-			fmt.Println("第二个条件分支被选中: chs[1]=>", num)
-		case num, ok := <-chs[2]:
-			if !ok {
-				break
-			}
-			fmt.Println("第三个条件分支被选中: chs[2]=>", num)
-		default:
-			fmt.Println("没有分支被选中")
-		}
-	}
-}
+//func main() {
+//	chs := [3]chan int{
+//		make(chan int, 3),
+//		make(chan int, 3),
+//		make(chan int, 3),
+//	}
+//
+//	rand.Seed(time.Now().UnixNano())
+//	index1 := rand.Intn(3) // 随机生成0-2之间的数字
+//	fmt.Printf("随机索引/数值: %d\n", index1)
+//	chs[index1] <- rand.Int() // 向通道发送随机数字
+//
+//	rand.Seed(time.Now().UnixNano())
+//	index2 := rand.Intn(3)
+//	fmt.Printf("随机索引/数值: %d\n", index2)
+//	chs[index2] <- rand.Int()
+//
+//	rand.Seed(time.Now().UnixNano())
+//	index3 := rand.Intn(3)
+//	fmt.Printf("随机索引/数值: %d\n", index3)
+//	chs[index3] <- rand.Int()
+//
+//	// 哪一个通道中有值，哪个对应的分支就会被执行
+//	for i := 0; i < 3; i++ {
+//		select {
+//		case num, ok := <-chs[0]:
+//			if !ok {
+//				break
+//			}
+//			fmt.Println("第一个条件分支被选中: chs[0]=>", num)
+//		case num, ok := <-chs[1]:
+//			if !ok {
+//				break
+//			}
+//			fmt.Println("第二个条件分支被选中: chs[1]=>", num)
+//		case num, ok := <-chs[2]:
+//			if !ok {
+//				break
+//			}
+//			fmt.Println("第三个条件分支被选中: chs[2]=>", num)
+//		default:
+//			fmt.Println("没有分支被选中")
+//		}
+//	}
+//}
