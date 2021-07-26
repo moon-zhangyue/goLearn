636ef249edc0c4f607716870f074a23620aced49
zhangyue <575865770@qq.com>
2021-07-26 11:20:02 +0800

    refactor: 读写锁

--- src/xiecheng/chapter09.go
+++ src/xiecheng/chapter09.go
@@ -25,10 +25,10 @@ func main() {
 	}
 
 	for {
-		lock.Lock()
+		lock.Lock() //防止读取阶段被其他并发操作更新
 		c := counter
 		lock.Unlock()
-		runtime.Gosched()
+		runtime.Gosched() //用于让出CPU时间片，让出当前goroutine的执行权限，调度器安排其它等待的任务运行，并在下次某个时候从该位置恢复执行
 		if c >= 10 {
 			break
 		}
