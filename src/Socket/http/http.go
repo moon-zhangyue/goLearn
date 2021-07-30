c4749e9e48271b784133caa5f15737aedc24886b
zhangyue <575865770@qq.com>
2021-07-30 13:43:57 +0800

    http请求  (*http.Client).Do

--- src/Socket/http/http.go
+++ src/Socket/http/http.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
-	"net/url"
 	"os"
 )
 
@@ -16,15 +16,38 @@ func main() {
 	//}
 
 	//http.PostForm
-	resp, err := http.PostForm("https://xueyuanjun.com/login", url.Values{"name": {"学院君"}, "password": {"test-passwd"}})
+	//resp, err := http.PostForm("https://xueyuanjun.com/login", url.Values{"name": {"学院君"}, "password": {"test-passwd"}})
+	//
+	//if err != nil {
+	//	// 处理错误
+	//	return
+	//}
+	//
+	//if resp.StatusCode != http.StatusOK {
+	//	// 处理错误
+	//	return
+	//}
+	//
+	//defer resp.Body.Close()
+	//io.Copy(os.Stdout, resp.Body)
 
+	//(*http.Client).Do
+
+	// 初始化客户端请求对象
+	req, err := http.NewRequest("GET", "https://xueyuanjun.com", nil)
 	if err != nil {
-		// 处理错误
+		fmt.Printf("请求初始化失败：%v", err)
 		return
 	}
-
-	if resp.StatusCode != http.StatusOK {
-		// 处理错误
+	// 添加自定义请求头
+	req.Header.Add("Custom-Header", "Custom-Value")
+	// ... 其它请求头配置
+	client := &http.Client{
+		// ... 设置客户端属性
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("客户端发起请求失败：%v", err)
 		return
 	}
 
