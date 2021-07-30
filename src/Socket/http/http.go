09674e5637ffc9fc847fe3b81d7a948ed18bd8e6
zhangyue <575865770@qq.com>
2021-07-30 11:11:31 +0800

    http请求  http.PostForm

--- src/Socket/http/http.go
+++ src/Socket/http/http.go
@@ -1,16 +1,30 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"os"
 )
 
 func main() {
-	resp, err := http.Get("https://xueyuanjun.com")
+	//http.Get
+	//resp, err := http.Get("https://xueyuanjun.com")
+	//if err != nil {
+	//	fmt.Printf("发起请求失败：%v", err)
+	//	return
+	//}
+
+	//http.PostForm
+	resp, err := http.PostForm("https://xueyuanjun.com/login", url.Values{"name": {"学院君"}, "password": {"test-passwd"}})
+
 	if err != nil {
-		fmt.Printf("发起请求失败：%v", err)
+		// 处理错误
+		return
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		// 处理错误
 		return
 	}
 
