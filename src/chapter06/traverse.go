33a509a1b9ad27cf8713ff1399aaf3c60e14ca26
zhangyue <575865770@qq.com>
2021-08-01 16:36:42 +0800

    后序遍历二叉树

--- src/chapter06/traverse.go
+++ src/chapter06/traverse.go
@@ -33,6 +33,34 @@ func preOrderTraverse(treeNode *Node) {
 	preOrderTraverse(treeNode.Right)
 }
 
+// 中序遍历
+func midOrderTraverse(treeNode *Node) {
+	// 节点为空则退出当前递归
+	if treeNode == nil {
+		return
+	}
+	// 否则先从左子树最左侧节点开始遍历
+	midOrderTraverse(treeNode.Left)
+	// 打印位于中间的根节点
+	fmt.Printf("%s ", treeNode.GetData())
+	// 最后按照和左子树一样的逻辑遍历右子树
+	midOrderTraverse(treeNode.Right)
+}
+
+// 后序遍历
+func postOrderTraverse(treeNode *Node) {
+	// 节点为空则退出当前递归
+	if treeNode == nil {
+		return
+	}
+	// 否则先遍历左子树
+	postOrderTraverse(treeNode.Left)
+	// 再遍历右子树
+	postOrderTraverse(treeNode.Right)
+	// 最后访问根节点
+	fmt.Printf("%s ", treeNode.GetData())
+}
+
 // 测试代码
 func main() {
 	// 初始化一个简单的二叉树
@@ -44,5 +72,15 @@ func main() {
 	// 前序遍历这个二叉树
 	fmt.Print("前序遍历: ")
 	preOrderTraverse(node1)
+	fmt.Println()
+
+	// 中序遍历这个二叉树
+	fmt.Print("中序遍历: ")
+	midOrderTraverse(node1)
+	fmt.Println()
+
+	// 后序遍历这个二叉树
+	fmt.Print("后序遍历: ")
+	postOrderTraverse(node1)
 	fmt.Println()
 }
