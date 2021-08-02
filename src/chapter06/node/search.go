0b8e8f3cd50c389411135815320a8ecf4fa6a82b
zhangyue <575865770@qq.com>
2021-08-02 14:05:06 +0800

    refactor: 二叉搜索树 删除

--- src/chapter06/node/search.go
+++ src/chapter06/node/search.go
@@ -70,6 +70,68 @@ func (tree *BinarySearchTree) Find(data int) *Node {
 	}
 }
 
+// Delete 删除值为 data 的节点
+func (tree *BinarySearchTree) Delete(data int) error {
+	if tree.root == nil {
+		return errors.New("二叉树为空")
+	}
+
+	p := tree.root
+	var pp *Node = nil // p 的父节点
+
+	// 找到待删除节点
+	for p != nil && p.Data.(int) != data {
+		pp = p
+		if p.Data.(int) < data {
+			// 当前节点值小于待删除值，去右子树查找
+			p = p.Right
+		} else {
+			// 否则去左子树查找
+			p = p.Left
+		}
+	}
+	if p == nil {
+		return errors.New("待删除节点不存在")
+	}
+
+	// 待删除节点有两个子节点，需要将待删除节点值设置为该节点右子树最小节点值，再删除右子树最小节点
+	if p.Left != nil && p.Right != nil {
+		minP := p.Right // 右子树中的最小节点
+		minPP := p      // minP 的父节点
+		// 查找右子树中的最小节点
+		for minP.Left != nil {
+			minPP = minP
+			minP = minP.Left
+		}
+		p.Data = minP.Data // 将 minP 的数据设置到 p 中
+		p = minP           // 下面就变成删除 minP 了
+		pp = minPP
+	}
+
+	// 应用待删除节点只有左/右子节点的逻辑
+	var child *Node
+	if p.Left != nil {
+		child = p.Left
+	} else if p.Right != nil {
+		child = p.Right
+	} else {
+		child = nil
+	}
+
+	if pp == nil {
+		// 删除跟节点
+		tree.root = nil
+	} else if pp.Left == p {
+		// 仅有左子节点
+		pp.Left = child
+	} else if pp.Right == p {
+		// 仅有右子节点
+		pp.Right = child
+	}
+
+	return nil
+}
+
 func main() {
 	tree := NewBinarySearchTree(nil)
 	_ = tree.Insert(3)
@@ -88,4 +150,16 @@ func main() {
 	fmt.Println("查找值为 10 的节点：")
 	node = tree.Find(10)
 	fmt.Printf("%v\n", node)
+
+	fmt.Println("删除值为 3 的节点：")
+	err := tree.Delete(3)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	} else {
+		fmt.Println("删除成功")
+	}
+
+	fmt.Println("中序遍历删除节点后的二叉排序树：")
+	midOrderTraverse(tree.root)
+	fmt.Println()
 }
