fe3845b10bea39f097b0d1128bdeecca36699d6a
zhangyue <575865770@qq.com>
2021-08-03 17:17:52 +0800

    go 回文数

--- src/leetcode/editor/cn/[9]回文数.go
+++ src/leetcode/editor/cn/[9]回文数.go
@@ -1,5 +1,6 @@
 package main
-//给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。 
+
+//给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
 //
 // 回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。例如，121 是回文，而 123 不是。 
 //
@@ -49,9 +50,25 @@ package main
 // Related Topics 数学 
 // 👍 1562 👎 0
 
-
 //leetcode submit region begin(Prohibit modification and deletion)
 func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
 
+	var y int
+	for {
+		y = y*10 + x%10
+		x = x / 10
+		if x == 0 {
+			break
+		}
+	}
+
+	if x != y {
+		return false
+	}
+	return true
 }
+
 //leetcode submit region end(Prohibit modification and deletion)
