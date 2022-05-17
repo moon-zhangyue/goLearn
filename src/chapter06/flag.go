854f015104b2541d81de79004faaf23a16d0840c
zhangyue <575865770@qq.com>
2022-05-17 15:39:18 +0800

    refctor: 重新提交 Flag 参数

---
+++ src/chapter06/flag.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"log"
+)
+
+/*func main() {
+	var name string
+	flag.StringVar(&name, "name", "Go 语言编程之旅", "帮助信息")
+	flag.StringVar(&name, "n", "Go 语言编程之旅", "帮助信息")
+	flag.Parse()
+
+	log.Printf("name: %s", name)
+}*/
+
+var name string
+
+func main() {
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) <= 0 {
+		return
+	}
+
+	switch args[0] {
+	case "go":
+		goCmd := flag.NewFlagSet("go", flag.ExitOnError)
+		goCmd.StringVar(&name, "name", "Go 语言", "帮助信息")
+		_ = goCmd.Parse(args[1:])
+	case "php":
+		phpCmd := flag.NewFlagSet("php", flag.ExitOnError)
+		phpCmd.StringVar(&name, "n", "PHP 语言", "帮助信息")
+		_ = phpCmd.Parse(args[1:])
+	}
+
+	log.Printf("name: %s", name)
+}
