3e027062c1b17bd96d2533d2d16e2688166a3ab1
zhangyue <575865770@qq.com>
2022-05-17 15:39:18 +0800

    refactor: Flag 参数

--- src/chapter06/test.go
+++ src/chapter06/test.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-)
-
 /*func main() {
 	var varI interface{} = 1
 
@@ -17,13 +12,13 @@ import (
 	}
 }*/
 
-func main() {
-	var p *int
-
-	b, err := json.Marshal(p)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(string(b))
-}
+//func main() {
+//	var p *int
+//
+//	b, err := json.Marshal(p)
+//	if err != nil {
+//		fmt.Println(err)
+//		return
+//	}
+//	fmt.Println(string(b))
+//}
