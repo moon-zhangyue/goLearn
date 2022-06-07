ed14e3a5bc69d4c8e268e5b473e41c30c9b82b38
zhangyue <575865770@qq.com>
2022-06-07 09:48:09 +0800

    refactor: go操作mysql Insert

---
+++ src/chapter06/mysql.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+)
+
+//database, err := sqlx.Open("mysql", "root:XXXX@tcp(127.0.0.1:3306)/test")
+//database, err := sqlx.Open("数据库类型", "用户名:密码@tcp(地址:端口)/数据库名")
+
+type Person struct {
+	UserId   int    `db:"user_id"`
+	Username string `db:"username"`
+	Sex      string `db:"sex"`
+	Email    string `db:"email"`
+}
+
+type Place struct {
+	Country string `db:"country"`
+	City    string `db:"city"`
+	TelCode int    `db:"telcode"`
+}
+
+var Db *sqlx.DB
+
+func init() {
+	database, err := sqlx.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/test")
+	if err != nil {
+		fmt.Println("open mysql failed,", err)
+		return
+	}
+	Db = database
+	//defer Db.Close() // 注意这行代码要写在上面err判断的下面
+	//需要关掉 下面还没有使用 链接 就关闭了  如果需要关闭 需要在处理完sql后再关闭
+}
+
+func main() {
+	r, err := Db.Exec("insert into person(username, sex, email)values(?, ?, ?)", "stu001", "man", "stu01@qq.com")
+	if err != nil {
+		fmt.Println("exec failed1, ", err)
+		return
+	}
+	id, err := r.LastInsertId()
+	if err != nil {
+		fmt.Println("exec failed2, ", err)
+		return
+	}
+
+	fmt.Println("insert succ:", id)
+}
