8697997997149ad30e3ff85d84949493946fbee0
zhangyue <575865770@qq.com>
2022-06-07 15:06:08 +0800

    refactor: go操作mysql 事务

--- src/chapter06/mysql.go
+++ src/chapter06/mysql.go
@@ -87,5 +87,39 @@ func main() {
 	fmt.Println("delete succ: ", row)*/
 
 	//事务
+	conn, err := Db.Begin() //开启事务
+	if err != nil {
+		fmt.Println("begin failed :", err)
+		return
+	}
 
+	r, err := conn.Exec("insert into person(username, sex, email)values(?, ?, ?)", "stu001", "man", "stu01@qq.com")
+	if err != nil {
+		fmt.Println("exec failed, ", err)
+		conn.Rollback() //回滚
+		return
+	}
+	id, err := r.LastInsertId()
+	if err != nil {
+		fmt.Println("exec failed, ", err)
+		conn.Rollback()
+		return
+	}
+	fmt.Println("insert succ:", id)
+
+	r, err = conn.Exec("insert into person(username, sex, email)values(?, ?, ?)", "stu001", "man", "stu01@qq.com")
+	if err != nil {
+		fmt.Println("exec failed, ", err)
+		conn.Rollback()
+		return
+	}
+	id, err = r.LastInsertId()
+	if err != nil {
+		fmt.Println("exec failed, ", err)
+		conn.Rollback()
+		return
+	}
+	fmt.Println("insert succ:", id)
+
+	conn.Commit() //提交
 }
